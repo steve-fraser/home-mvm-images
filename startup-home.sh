c2cba7d88e04a1a7ad7e573b907508be83f0adc0
Steven Fraser <steve.fraser@weave.works>
2022-07-31 23:58:30 -0400

    update start up script

--- startup-home.sh
+++ startup-home.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /mnt/plex
-sudo mount -t nfs 192.168.7.67:/mnt/user/plex /mnt/plex
+sudo mount -t nfs 192.168.0.16:/mnt/plex /mnt/plex 
 mkdir -p /mnt/appdata
-sudo mount -t nfs 192.168.7.67:/mnt/user/appdata /mnt/appdata
+sudo mount -t nfs 192.168.0.16:/mnt/appdata /mnt/appdata 
